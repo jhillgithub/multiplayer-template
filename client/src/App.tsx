fd9715d842163d77e70ffa5ca395589faa9c0598
jh <>
2024-04-25 23:00:59 -0500

    cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,23 @@
-import "./App.css";
+import { Home } from "./pages/Home.tsx";
+import { Room } from "./pages/Room.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RoomContextProvider } from "./context/RoomContext.tsx";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <RoomContextProvider>
+          <div className="container mx-auto w-screen h-screen mt-40">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/room/:roomId" element={<Room />} />
+            </Routes>
+          </div>
+        </RoomContextProvider>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ app.use(cors);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: '*', // @WARNING: For debug use only
     methods: ['GET', 'POST'],
   },
 });
